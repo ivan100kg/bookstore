3ed8f7a2ca7fae2281a8e0b2c340f6b02ba69019
Ivar <ivar110kg@gmail.com>
2021-10-10 18:58:20 +0400

    add filters/searching/ordering

--- books/store/models.py
+++ books/store/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     author = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return f'{self.author}: {self.name}'
