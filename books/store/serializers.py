898a2f1f0ce9a07399e04f84e35e0f58aa768c7d
ivar <ivar110kg@gmail.com>
2021-10-17 18:25:00 +0400

    update serializers.py

--- books/store/serializers.py
+++ books/store/serializers.py
@@ -6,11 +6,11 @@ from store.models import Book, UserBookRelation
 
 class BooksSerializer(ModelSerializer):
     likes_count = serializers.SerializerMethodField()
-    annotated_likes = serializers.ImageField()
+    # annotated_likes = serializers.ImageField()
 
     class Meta:
         model = Book
-        fields = ['id', 'name', 'price', 'author', 'likes_count', 'annotated_likes']
+        fields = ['id', 'name', 'price', 'author', 'likes_count']
 
     def get_likes_count(self, instance):
         return UserBookRelation.objects.filter(book=instance, like=True).count()
